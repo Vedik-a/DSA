1cc86c6c77d1c7b466ee1f58e244d5d91983f586
vedika <152393258+Vedik-a@users.noreply.github.com>
2024-11-16 09:47:13 +0530

    Add files via upload

---
+++ string/StringPalin.java
@@ -0,0 +1,31 @@
+public class StringPalin {
+
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        StringPalin obj = new StringPalin(); // Updated to match the class name
+        System.out.println(obj.isPalindrome("A man, a plan, a canal: Panama")); // Output should be true
+    }
+}
